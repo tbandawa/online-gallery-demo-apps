1e7f25a04c8051728ad5bfb0c3a47a31c38ccc84
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-03-02 23:58:45 +0200

    implement fetch gallery and return network result wrapper class

--- app/src/main/java/me/tbandawa/android/gallerydemo/data/repository/GalleryRepository.kt
+++ app/src/main/java/me/tbandawa/android/gallerydemo/data/repository/GalleryRepository.kt
@@ -2,10 +2,10 @@ package me.tbandawa.android.gallerydemo.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import me.tbandawa.android.gallerydemo.data.model.Gallery
-import me.tbandawa.android.gallerydemo.data.model.Resource
+import me.tbandawa.android.gallerydemo.data.model.NetworkResult
 
 interface GalleryRepository {
 
-    suspend fun fetchGallery(): Flow<Resource<List<Gallery>>>
+    suspend fun fetchGallery(): Flow<NetworkResult<List<Gallery>>>
 
 }
--- app/src/main/java/me/tbandawa/android/gallerydemo/data/repository/GalleryRepositoryImpl.kt
+++ app/src/main/java/me/tbandawa/android/gallerydemo/data/repository/GalleryRepositoryImpl.kt
@@ -1,17 +1,23 @@
 package me.tbandawa.android.gallerydemo.data.repository
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import me.tbandawa.android.gallerydemo.data.api.GalleryApi
 import me.tbandawa.android.gallerydemo.data.model.Gallery
-import me.tbandawa.android.gallerydemo.data.model.Resource
+import me.tbandawa.android.gallerydemo.data.model.NetworkResult
 import javax.inject.Inject
 
 class GalleryRepositoryImpl @Inject constructor(
-    val galleryApi: GalleryApi
-): GalleryRepository {
+    private val galleryApi: GalleryApi
+): ApiCallHelper(), GalleryRepository {
 
-    override suspend fun fetchGallery(): Flow<Resource<List<Gallery>>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchGallery(): Flow<NetworkResult<List<Gallery>>> = flow {
+        emit(NetworkResult.Loading)
+        emit(safeApiCall {
+            galleryApi.fetchGallery()
+        })
+    }.flowOn(Dispatchers.IO)
 
 }
