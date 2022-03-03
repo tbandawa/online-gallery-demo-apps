3f5487edec452b3a53ef686fe3d1613300dd334c
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-03-03 19:51:57 +0200

    add gallery repository dependency

--- app/src/main/java/me/tbandawa/android/gallerydemo/di/DataModule.kt
+++ app/src/main/java/me/tbandawa/android/gallerydemo/di/DataModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import me.tbandawa.android.gallerydemo.data.api.GalleryApi
 import me.tbandawa.android.gallerydemo.data.api.RetrofitHelper
+import me.tbandawa.android.gallerydemo.data.repository.GalleryRepository
+import me.tbandawa.android.gallerydemo.data.repository.GalleryRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -15,5 +17,9 @@ object DataModule {
     @Provides
     @Singleton
     fun provideGalleryApi(): GalleryApi = RetrofitHelper.getInstance().create(GalleryApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideGalleryRepository(galleryApi: GalleryApi): GalleryRepository = GalleryRepositoryImpl(galleryApi)
 
 }
