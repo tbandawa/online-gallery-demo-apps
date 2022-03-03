20d3f13e5a5a6d910f29c084b653b284cb7da0b9
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-03-03 19:27:05 +0200

    add get single gallery interface

--- app/src/main/java/me/tbandawa/android/gallerydemo/data/api/GalleryApi.kt
+++ app/src/main/java/me/tbandawa/android/gallerydemo/data/api/GalleryApi.kt
@@ -3,10 +3,14 @@ package me.tbandawa.android.gallerydemo.data.api
 import me.tbandawa.android.gallerydemo.data.model.Gallery
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GalleryApi {
 
     @GET("gallery")
     suspend fun fetchGallery() : Response<List<Gallery>>
+
+    @GET("/{id}/")
+    suspend fun fetchResources(@Path("id") id: Long) : Response<Gallery>
 
 }
