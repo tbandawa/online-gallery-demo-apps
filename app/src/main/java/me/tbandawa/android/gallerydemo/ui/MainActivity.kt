26d67a646294b96069d2b720412cc20d9dcd94a2
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-05-15 16:01:59 +0200

    add theming and branding

--- app/src/main/java/me/tbandawa/android/gallerydemo/ui/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/gallerydemo/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package me.tbandawa.android.gallerydemo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,11 +15,18 @@ import me.tbandawa.android.gallerydemo.ui.screens.notifications.NotificationsScr
 import me.tbandawa.android.gallerydemo.ui.screens.profile.EditProfileScreen
 import me.tbandawa.android.gallerydemo.ui.screens.profile.ProfileScreen
 import me.tbandawa.android.gallerydemo.ui.screens.profile.SettingsScreen
+import me.tbandawa.android.gallerydemo.ui.screens.splash.SplashViewModel
 import me.tbandawa.android.gallerydemo.ui.theme.GalleryDemoTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
+
         super.onCreate(savedInstanceState)
         setContent {
 
