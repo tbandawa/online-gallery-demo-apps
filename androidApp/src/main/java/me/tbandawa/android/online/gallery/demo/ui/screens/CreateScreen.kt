d854258f0b6b8642474c7a5289ca0f99d8f8aa5f
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-01-18 07:57:30 +0200

    scrollable view

--- androidApp/src/main/java/me/tbandawa/android/online/gallery/demo/ui/screens/CreateScreen.kt
+++ androidApp/src/main/java/me/tbandawa/android/online/gallery/demo/ui/screens/CreateScreen.kt
@@ -9,16 +9,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -32,7 +27,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.rememberImagePainter
-import coil.size.OriginalSize
 import me.tbandawa.android.online.gallery.R
 import me.tbandawa.android.online.gallery.demo.ui.components.MainToolbar
 import java.util.*
@@ -69,124 +63,117 @@ fun CreateScreen(
             },
             containerColor = Color.White
         ) { it ->
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(it)
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
             ) {
-                Column(
+                TextField(
+                    value = textTitle,
+                    singleLine = true,
+                    enabled = !isLoading,
+                    onValueChange = { input ->
+                        textTitle = input
+                        isTitleValid = input.text.isNotBlank()
+                    },
+                    placeholder = { Text(text = "Title") },
                     modifier = Modifier
-                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .background(
+                            color = Color(0xffF0F5F1),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .border(
+                            width = 2.dp,
+                            color = if (isTitleValid) Color.Transparent else Color.Red,
+                            shape = RoundedCornerShape(10.dp)
+                        ),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedTextColor = Color(0xff024040),
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        unfocusedPlaceholderColor = if (isTitleValid) Color(0x90024040) else Color(0xfff55050),
+                        disabledLeadingIconColor = Color(0xff024040)
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(25.dp))
+                TextField(
+                    value = textDescription,
+                    singleLine = false,
+                    maxLines = 4,
+                    minLines = 4,
+                    enabled = !isLoading,
+                    onValueChange = { input ->
+                        textDescription = input
+                        isDescriptionValid = input.text.isNotBlank()
+                    },
+                    placeholder = { Text(text = "Description") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .background(
+                            color = Color(0xffF0F5F1),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .border(
+                            width = 2.dp,
+                            color = if (isDescriptionValid) Color.Transparent else Color.Red,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                    ,
+                    shape = RoundedCornerShape(10.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedTextColor = Color(0xff024040),
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        unfocusedPlaceholderColor = if (isDescriptionValid) Color(0x90024040) else Color(0xfff55050),
+                        disabledLeadingIconColor = Color(0xff024040)
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(25.dp))
+                Text(
+                    text = "Images",
+                    style = TextStyle(
+                        color = Color(0xff024040),
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 16.sp
+                    ),
+                    modifier = Modifier.padding(bottom = 5.dp)
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    Column {
-
-                        TextField(
-                            value = textTitle,
-                            singleLine = true,
-                            enabled = !isLoading,
-                            onValueChange = { input ->
-                                textTitle = input
-                                isTitleValid = input.text.isNotBlank()
+                    selectImages.forEach { uri ->
+                        ImageFile(uri = uri)
+                    }
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 0.dp)
+                    ) {
+                        FilledTonalButton(
+                            onClick = {
+                                galleryLauncher.launch("image/*")
                             },
-                            placeholder = { Text(text = "Title") },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(50.dp)
-                                .background(
-                                    color = Color(0xffF0F5F1),
-                                    shape = RoundedCornerShape(10.dp)
-                                )
-                                .border(
-                                    width = 2.dp,
-                                    color = if (isTitleValid) Color.Transparent else Color.Red,
-                                    shape = RoundedCornerShape(10.dp)
-                                ),
-                            shape = RoundedCornerShape(10.dp),
-                            colors = TextFieldDefaults.colors(
-                                focusedTextColor = Color(0xff024040),
-                                focusedIndicatorColor = Color.Transparent,
-                                unfocusedIndicatorColor = Color.Transparent,
-                                disabledIndicatorColor = Color.Transparent,
-                                unfocusedPlaceholderColor = if (isTitleValid) Color(0x90024040) else Color(0xfff55050),
-                                disabledLeadingIconColor = Color(0xff024040)
-                            )
-                        )
-
-                        Spacer(modifier = Modifier.height(25.dp))
-                        TextField(
-                            value = textDescription,
-                            singleLine = false,
-                            maxLines = 4,
-                            minLines = 4,
-                            enabled = !isLoading,
-                            onValueChange = { input ->
-                                textDescription = input
-                                isDescriptionValid = input.text.isNotBlank()
-                            },
-                            placeholder = { Text(text = "Description") },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .wrapContentHeight()
-                                .background(
-                                    color = Color(0xffF0F5F1),
-                                    shape = RoundedCornerShape(10.dp)
-                                )
-                                .border(
-                                    width = 2.dp,
-                                    color = if (isDescriptionValid) Color.Transparent else Color.Red,
-                                    shape = RoundedCornerShape(10.dp)
-                                )
-                            ,
-                            shape = RoundedCornerShape(10.dp),
-                            colors = TextFieldDefaults.colors(
-                                focusedTextColor = Color(0xff024040),
-                                focusedIndicatorColor = Color.Transparent,
-                                unfocusedIndicatorColor = Color.Transparent,
-                                disabledIndicatorColor = Color.Transparent,
-                                unfocusedPlaceholderColor = if (isDescriptionValid) Color(0x90024040) else Color(0xfff55050),
-                                disabledLeadingIconColor = Color(0xff024040)
-                            )
-                        )
-
-                        Spacer(modifier = Modifier.height(25.dp))
-                        Text(
-                            text = "Images",
-                            style = TextStyle(
-                                color = Color(0xff024040),
-                                fontWeight = FontWeight.Medium,
-                                fontSize = 16.sp
-                            ),
-                            modifier = Modifier.padding(bottom = 5.dp)
-                        )
-                        Column(
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                            shape = RoundedCornerShape(25)
                         ) {
-                            selectImages.forEach { uri ->
-                                ImageFile(uri = uri)
-                            }
-                            Box(
-                                modifier = Modifier
-                                    .padding(top = 0.dp)
-                            ) {
-                                FilledTonalButton(
-                                    onClick = {
-                                        galleryLauncher.launch("image/*")
-                                    },
-                                    shape = RoundedCornerShape(25)
-                                ) {
-                                    Image(
-                                        painterResource(id = R.drawable.ic_add),
-                                        contentDescription ="Add Image",
-                                        modifier = Modifier.size(20.dp))
-                                    Text(
-                                        text = "Add to cart",
-                                        Modifier
-                                            .padding(start = 10.dp)
-                                    )
-                                }
-                            }
+                            Image(
+                                painterResource(id = R.drawable.ic_add),
+                                contentDescription ="Add Image",
+                                modifier = Modifier.size(20.dp))
+                            Text(
+                                text = "Add to cart",
+                                Modifier
+                                    .padding(start = 10.dp)
+                            )
                         }
-                        
                     }
                 }
             }
