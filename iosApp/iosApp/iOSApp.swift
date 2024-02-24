78ae47b7aed4397bf3f01128e4aa9160a38ab77d
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-02-24 14:51:55 +0200

    init koin from iOS without using delegate

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -4,18 +4,13 @@ import data
 @main
 struct iOSApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    init() {
+        DataModuleKt.doInitKoin()
+    }
     
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 		}
 	}
-}
-
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        DataModuleKt.doInitKoin()
-        return true
-    }
 }
