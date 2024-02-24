2e6d8c538784cbb4d2343d7d3a7dcbb226fa1173
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-02-24 15:17:07 +0200

    add groups and auth views

---
+++ iosApp/iosApp/Views/Auth/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  Online Gallery
+//
+//  Created by Tendai Bandawa on 2024/02/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ iosApp/iosApp/Views/Auth/RegisterView.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterView.swift
+//  Online Gallery
+//
+//  Created by Tendai Bandawa on 2024/02/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
